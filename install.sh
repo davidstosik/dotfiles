815523743a580d0eb7c2bf700a5527faacd9ac25
David Stosik <david.stosik+git-noreply@gmail.com>
2017-02-16 18:34:30 +0900

    Add Homebrew packages step to install.

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo "Installing dotfiles"
 
+source install/brew.sh
+
 source install/backup.sh
 
 source install/vim.sh
---
+++ install/brew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install some basic utilities
+brew tap homebrew/dupes
+brew install bash-completion git hub
+brew install grep --with-default-names

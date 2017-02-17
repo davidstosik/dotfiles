4fe846fde263d1b2c8a24cd195555ca65ba67447
David Stosik <david.stosik+git-noreply@gmail.com>
2017-02-17 11:01:44 +0900

    Install all vim Plugins during install.
    Also prevent error message when colorscheme can't be loaded

--- install/vim.sh
+++ install/vim.sh
@@ -4,3 +4,5 @@ echo "Preparing vim"
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall

cc9e1616780839fe46b6fd0c6e2e7f132fbe9143
David Stosik <david.stosik+git-noreply@gmail.com>
2019-02-25 23:15:06 +0900

    Upgrade all Homebrew packages at beginning so that scripts don't have to later

--- helpers/common.sh
+++ helpers/common.sh
@@ -12,19 +12,9 @@ is_macos() {
 
 install_package() {
   if is_macos; then
-    brew_update_or_install $1
+    brew install $1
   elif is_termux; then
     echo "Installing $1..."
     pkg install -y $1
-  fi
-}
-
-brew_update_or_install() {
-  if brew ls --versions $1 > /dev/null && ! brew outdated $1 > /dev/null; then
-    echo "Updating $1..."
-    brew upgrade $1
-  else
-    echo "Installing $1..."
-    brew install $1
   fi
 }

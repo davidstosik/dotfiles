d9a5c5252d38f4a1028bb128f62fe9fe84a16dfc
David Stosik <david.stosik+git-noreply@gmail.com>
2021-01-15 17:49:50 +0900

    Add minimal Homebrew install

---
+++ modules/005-homebrew/install.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Skip Homebrew install if not macOS
+[ "`uname`" = "Darwin" ] || exit
+
+if command -v brew >> /dev/null; then
+  echo "== Updating Homebrew =="
+  brew update
+else
+  # [TODO] Consider whether to install Homebrew inside user's home directory
+  echo "== Installing Homebrew =="
+  echo "== (This may require temporary sudo access) =="
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi

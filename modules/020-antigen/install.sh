ef2d5e770b686cbd544b6f6f9c65abbd8b2086e8
David Stosik <david.stosik+git-noreply@gmail.com>
2022-02-07 14:36:23 +0900

    Fix Antigen bug caused by spaces in paths
    See https://github.com/zsh-users/antigen/issues/734

--- modules/020-antigen/install.sh
+++ modules/020-antigen/install.sh
@@ -1,12 +1,17 @@
 ##!/usr/bin/env zsh
 
+MYDIR="${0:a:h}"
+
 ANTIGEN_ROOT="${HOME}/.antigen"
 
 # TODO handle existance?
 git clone https://github.com/zsh-users/antigen "${ANTIGEN_ROOT}/git"
 
+# Fix Antigen bug caused by spaces in paths
+# See https://github.com/zsh-users/antigen/issues/734
+git -C "${ANTIGEN_ROOT}/git" apply "${MYDIR}/fix_spaces.patch"
+
 # prepare data file for Z
 touch "${ANTIGEN_ROOT}/.z"
 
-MYDIR="${0:a:h}"
 source "${MYDIR}/zshrc"

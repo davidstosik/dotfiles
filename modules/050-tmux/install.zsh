0f1f60b9856c64584f53f850e9d615c6b198299e
David Stosik <david.stosik@shopify.com>
2022-02-07 07:40:36 +0000

    Add tmux settings

---
+++ modules/050-tmux/install.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+MYDIR="${0:a:h}"
+
+command_exists() {
+  command -v $@ >/dev/null 2>&1
+}
+
+if ! command_exists tmux; then
+  if command_exists brew; then
+    brew install tmux
+  elif command_exists apt-get; then
+    apt-get install -y tmux
+  else
+    echo "Unable to install tmux, skipping..."
+    exit 1
+  fi
+fi
+
+# TODO avoid failing if already exists
+ln -s "${MYDIR}/tmux.conf" "$HOME"/.tmux.conf
+
+echo "== Installing Tmux Plugin Manager (TPM) =="
+TPM="${HOME}/.tmux/plugins/tpm"
+mkdir -p "$TPM"
+
+# TODO handle existance
+git clone https://github.com/tmux-plugins/tpm "$TPM"
+
+echo "== Installing TPM plugins =="
+${TPM}/bin/install_plugins

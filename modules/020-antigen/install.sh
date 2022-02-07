b412e90d2db69dba58d53b15ceaa95c7c5b992d0
David Stosik <david.stosik+git-noreply@gmail.com>
2022-02-08 00:11:42 +0900

    Add antigen as submodule instead of cloning it

--- modules/020-antigen/install.sh
+++ modules/020-antigen/install.sh
@@ -2,16 +2,8 @@
 
 MYDIR="${0:a:h}"
 
-ANTIGEN_ROOT="${HOME}/.antigen"
-
-# TODO handle existance?
-git clone https://github.com/zsh-users/antigen "${ANTIGEN_ROOT}/git"
-
 # Fix Antigen bug caused by spaces in paths
 # See https://github.com/zsh-users/antigen/issues/734
-git -C "${ANTIGEN_ROOT}/git" apply "${MYDIR}/fix_spaces.patch"
-
-# prepare data file for Z
-touch "${ANTIGEN_ROOT}/.z"
+git -C "${MYDIR}/../../submodules/antigen" apply "${MYDIR}/fix_spaces.patch"
 
 source "${MYDIR}/zshrc"

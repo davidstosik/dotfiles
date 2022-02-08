ce9a2adb84f56d091edb7cefdc807195cb1e797d
David Stosik <david.stosik+git-noreply@gmail.com>
2022-02-08 11:54:14 +0900

    Simplify FZF setup

--- modules/600-fzf/install.sh
+++ modules/600-fzf/install.sh
@@ -2,3 +2,7 @@
 
 echo "== Installing FZF binary =="
 ${0:a:h}/../../submodules/fzf/install --bin
+
+echo "== Linking FZF into vim-plug's directory =="
+rm -rf "$HOME"/.vim/plugged/fzf
+ln -s ${0:a:h}/../../submodules/fzf "$HOME"/.vim/plugged

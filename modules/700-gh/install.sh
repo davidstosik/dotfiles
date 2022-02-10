a8c778d24a3cd9f037ef4b6e7ccfec4001bf2d68
David Stosik <davidstosik@users.noreply.github.com>
2022-02-10 19:51:35 +0900

    Install GitHub CLI

--- modules/700-gh/install.sh
+++ modules/700-gh/install.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env zsh
 
-#TODO install gh if not installed
+command_exists() {
+  command -v $@ >/dev/null 2>&1
+}
+
+if ! command_exists gh; then
+  if command_exists apt-get; then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y gh
+  else
+    echo "Unable to install gh on this platform."
+  fi
+fi
 
 if command -v gh >/dev/null 2>&1; then
   gh completion -s zsh > "$HOME"/.zsh_functions/_gh

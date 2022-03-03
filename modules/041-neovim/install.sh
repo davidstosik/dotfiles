b817599c4de5acc2ba67e43b92846d491aec7979
David Stosik <davidstosik@users.noreply.github.com>
2022-03-04 00:06:53 +0900

    Instal NeoVim alongside Vim

---
+++ modules/041-neovim/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+if ! command -v nvim >/dev/null 2>&1; then
+  if [[ "`uname -sm`" == "Linux x86_64" ]]; then
+    mkdir "$HOME"/.nvim
+    curl --silent --location https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -o - | tar xvzf - -C "$HOME"/.nvim
+    ln -s "$HOME"/.nvim/nvim-linux64/bin/nvim "$HOME"/bin
+  else
+    echo "Don't know how to install NeoVim on this platform!"
+  fi
+fi
+
+MYDIR="${0:a:h}"
+
+mkdir -p "$HOME"/.config/nvim
+ln -s "${MYDIR}/init.vim" "$HOME"/.config/nvim/

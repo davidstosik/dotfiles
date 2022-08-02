4a70e58b4d495c4b3f9f7ee4140a93a6be499d1e
David Stosik <david.stosik+git-noreply@gmail.com>
2022-08-02 10:31:47 +0900

    Download evicted iCloud dotfiles on login

---
+++ modules/999-icloud/install.sh
@@ -0,0 +1,12 @@
+if [[ "$(uname -s)" != "Darwin" ]]; then
+  exit 0
+else
+  echo "== Setting up iCloud Drive related tools =="
+fi
+
+MYDIR="${0:a:h}"
+
+mkdir -p "$HOME/bin"
+
+ln -fs "$MYDIR/find-evicted-icloud-files" "$HOME/bin/"
+ln -fs "$MYDIR/download-evicted-icloud-files" "$HOME/bin/"

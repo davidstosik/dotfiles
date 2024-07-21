8bd22f874b8ab50ad8dc3914ac8678e106300afa
David Stosik <david.stosik+git-noreply@gmail.com>
2024-07-21 15:04:46 +0900

    Disable LazyVim autoformat by default

--- modules/041-neovim/config-nvim/lua/config/options.lua
+++ modules/041-neovim/config-nvim/lua/config/options.lua
@@ -4,3 +4,4 @@
 vim.g.ruby_host_prog = '/home/spin/.gem/ruby/3.3.0/bin/neovim-ruby-host'
 vim.opt.clipboard = vim.env.SSH_TTY and "unnamedplus" or
 "unnamedplus"                                                          -- Sync with system clipboard -- FIXME only for spin?
+vim.g.autoformat = false

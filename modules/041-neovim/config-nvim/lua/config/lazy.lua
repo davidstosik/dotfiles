fbf4a0ec6b7e76f4b1708989e117fee7b5b299fc
David Stosik <david.stosik+git-noreply@gmail.com>
2024-07-21 15:03:30 +0900

    Run LazyVim updater once a day at most

--- modules/041-neovim/config-nvim/lua/config/lazy.lua
+++ modules/041-neovim/config-nvim/lua/config/lazy.lua
@@ -28,7 +28,11 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true,
+    notify = true,
+    frequency = 86400, -- 1 day
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
